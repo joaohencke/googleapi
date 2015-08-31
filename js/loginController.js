e08a9bbae66405db5804dd63dc35a30549f03328
Joao Hencke <joao.hencke@gmail.com>
2015-08-31 11:36:59 -0300

    removed unused dependencies and api.js import from index

--- js/loginController.js
+++ js/loginController.js
@@ -1,4 +1,4 @@
-app.controller('loginController', ['$scope', '$location', 'GAPI','Plus', function ($scope, $location, GAPI, Plus) { 
+app.controller('loginController', ['$scope', '$location', 'GAPI', function ($scope, $location, GAPI) { 
 	$scope.authenticate = function () {
 		var promise = GAPI.init();
 		promise.then(function(resp) {
